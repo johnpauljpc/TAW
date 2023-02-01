2e60a620151aca6a92b60360442cd4bd3c3f2196
johnpauljpc <jpcwork081@gmail.com>
2023-02-01 16:44:38 +0100

    reviews

--- js/script.js
+++ js/script.js
@@ -40,3 +40,15 @@ themeBtn.onclick = () =>{
     }
 }
 
+var swiper = new Swiper(".review-slider", {
+    loop: true,
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
